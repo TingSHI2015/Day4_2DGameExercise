fa06781afc1ec27b9e95220cbc4404188fb8cd45
tingSHI <ting.shi2015@gmail.com>
2024-04-18 17:47:00 +0200

    4 Version: Full functions

--- src/main/java/PlayerCharacter.java
+++ src/main/java/PlayerCharacter.java
@@ -1,15 +1,48 @@
+import static java.nio.file.Files.move;
+
 public class PlayerCharacter {
 
+    // Class-level variables: here must use 'static'
+    static int xPosition = 0;  // 1. not 'int static'!! 2. 'public static int xPosition = 0' also ok!
+    static int yPosition = 0;
+
+    //Florian's code doesn't have 'main' method
     public static void main(String[] args) {
     }
 
-    public static int getX(int value){
-        return 0;
+
+ //   public static int getX(int value){
+    public static int getX(){
+        return xPosition;
     }
 
-    public static int getY(int value){
-        return 0;
+//    public static int getY(int value){
+    public static int getY(){
+        return yPosition;
     }
+
+    public static int move(String direction){
+        if (direction == "W"){   //teacher code: direction.equals("W")
+            yPosition++;
+            return getY();      //nicht gut: return yPosition;
+        }
+        else if(direction.equals("S")){         //direction == "S"
+            yPosition--;
+            return getY();
+        }
+        else if(direction == "D"){
+            xPosition++;
+            return getX();     //nicht gut: return xPosition;
+        }
+        else if(direction == "A"){
+            xPosition--;
+            return getX();
+        }
+        else
+            return 100;     // if forget else (z.B. direction = "Z", then also need to return sth. int!)
+
+     }
+
 
 
 
--- src/test/java/PlayerCharacterTest.java
+++ src/test/java/PlayerCharacterTest.java
@@ -1,35 +1,124 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PlayerCharacterTest {
+
+    @BeforeEach
+    public void setUp() {
+        PlayerCharacter.xPosition = 0;  //worry: PlayerCharacter.getX() = 0;
+        PlayerCharacter.yPosition = 0;  //worry: PlayerCharacter.getY() = 0;
+    }
 
     @Test
     public void testGetXReturnsZeroInitially() {
 
         //GIVEN
-        int value=0;
+        //  int value=0;
 
         //WHEN
-        int actualResult = PlayerCharacter.getX(value);
+        //int actualResult = PlayerCharacter.getX(value);
+        int actual = PlayerCharacter.getX();
 
         //THEN
-        int expectedResult = 0;
-        assertEquals(expectedResult, actualResult, "test failed for testGetXReturnsZeroInitially");
+        int expected = 0;
+        assertEquals(expected, actual, "test failed for testGetXReturnsZeroInitially");
     }
 
     @Test
     public void testGetYReturnsZeroInitially() {
         //GIVEN
-        int value=0;
+        //int value=0;
 
         //WHEN
-        int actualResult = PlayerCharacter.getY(value);
+        //int actual = PlayerCharacter.getY(value);
+        int actual = PlayerCharacter.getY();
 
         //THEN
-        int expectedResult = 0;
-        assertEquals(expectedResult, actualResult, "test failed for testGetYReturnsZeroInitially");
+        int expected = 0;
+
+        //don't forget assert at the end!!!!!!
+        assertEquals(expected, actual, "test failed for testGetYReturnsZeroInitially");
 
     }
+
+
+    @Test
+    public void testMoveUpWithW() {
+        //GIVEN
+        PlayerCharacter.move("W");
+
+        //WHEN
+        int actual = PlayerCharacter.getY();
+
+        //THEN
+        int expected = 1;
+        assertEquals(expected, actual, "test failed for testMoveWithW");
+
+    }
+
+    @Test
+    public void testMoveDownWithS() {
+        //GIVEN
+        PlayerCharacter.move("S");
+
+        //WHEN
+        int actual = PlayerCharacter.getY();
+
+        //THEN
+        int expected = -1;
+        assertEquals(expected,actual,"test failed for testMoveDownWithS");
+    }
+
+    @Test
+    public void testMoveRightWithD() {
+        //GIVEN
+        PlayerCharacter.move("D");
+
+        //WHEN
+        int actual = PlayerCharacter.getX();
+
+        //THEN
+        int expected = 1;
+        assertEquals(expected,actual,"test failed for testMoveRightWithD");
+
+    }
+
+    @Test
+    public void testMoveLeftWithA() {
+        //GIVEN
+        PlayerCharacter.move("A");
+
+        //WHEN
+        int actual = PlayerCharacter.getX();
+
+        //THEN
+        int expected = -1;
+        assertEquals(expected,actual,"test failed for testMoveLeftWithA");
+
+    }
+
+
+    @Test
+    public void testWhenInputIsNotWASD_ShouldReturn100() {     //don't forget this test!!!!
+        //GIVEN
+        PlayerCharacter.move("X");
+
+        //WHEN
+        int actual = 100;
+
+        //THEN
+        assertTrue(actual == 100, "test failed for testWhenInputIsNotWASD_ShouldReturn100");
+
+
+
+    }
+
+
+
+
 
 }
